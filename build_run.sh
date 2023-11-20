ad06737964bc9726335ef7d79521f7f01f1ff4a3
Pi-Farm <43029891+pi-farm@users.noreply.github.com>
2023-11-20 01:41:59 +0100

    format

--- build_run.sh
+++ build_run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-docker stop pxe-container-s6
-docker rm pxe-container-s6
-docker rmi pxe-image:s6-test01
+docker stop pxe-container
+docker rm pxe-container
+docker rmi pxe-image:latest
 
-docker build -t pxe-image:s6-test01 .
-#docker run -it --privileged --net=host --volume ${PWD}/RPi-PXE-Server:/app/RPi-PXE-Server --volume ${PWD}/srv:/srv --name pxe-container-s6 pxe-image:s6-test01
-##docker exec -it pxe-container-s6 bash
+docker build -t pxe-image:latest .
